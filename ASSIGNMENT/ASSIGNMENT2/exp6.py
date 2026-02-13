b2d39b7b363f8c93f8f84554d001a6305aa9e9d6
Sujal gautam <sujalgautam07@gmail.com>
2026-02-13 11:32:43 +0530

    Create exp6.py

---
+++ ASSIGNMENT/ASSIGNMENT2/exp6.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, arr):
+        n = len(arr)
+        
+        last = arr[n - 1]
+        
+        for i in range(n - 1, 0, -1):
+            arr[i] = arr[i - 1]
+            
+        arr[0] = last
